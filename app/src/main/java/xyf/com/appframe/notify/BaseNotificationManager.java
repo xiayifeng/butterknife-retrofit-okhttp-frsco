a6cb2c52691df82f80f457339faa5c66cfe75dc2
sh-xiayf <yifeng.xia>
2016-10-12 10:22:27 +0800

    test

--- app/src/main/java/xyf/com/appframe/notify/BaseNotificationManager.java
+++ app/src/main/java/xyf/com/appframe/notify/BaseNotificationManager.java
@@ -1,7 +1,14 @@
 package xyf.com.appframe.notify;
 
+import android.app.Notification;
+
 /**
  * Created by sh-xiayf on 16/10/8.
  */
-public class BaseNotificationManager {
+public abstract class BaseNotificationManager {
+
+    public abstract Notification create(BaseNotifyData data);
+
+    public abstract void notify(Notification notification,int type);
+
 }
--- app/src/main/java/xyf/com/appframe/notify/BaseNotifyData.java
+++ app/src/main/java/xyf/com/appframe/notify/BaseNotifyData.java
@@ -3,5 +3,5 @@ package xyf.com.appframe.notify;
 /**
  * Created by sh-xiayf on 16/10/8.
  */
-public class BaseNotifyData {
+public abstract class BaseNotifyData {
 }
